b85cf5483b06646485035b04a0ac6308b46cf12c
Pascal Laenen <plaenen@gmail.comn>
2014-09-20 13:01:17 +0100

    adding genomes service - draft

---
+++ micro-services/geonames/server.js
@@ -0,0 +1,11 @@
+var argh = require('argh');
+
+var opts = {
+    'username': argh.argv.username || process.env.MSGN_USERNAME,
+    'port' : argh.argv.port || process.env.MSGN_PORT || 3000
+};
+
+var geonames = require('node-geonames-client')({username:opts.username});
+
+geoname.
+
---
+++ start.sh
@@ -0,0 +1,2 @@
+redis-server /usr/local/etc/redis.conf
+node ./micro-services/geonames/server.js --username:plaenen
